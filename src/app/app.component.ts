dd3b0e47686874cf80f0c346d48271351c9ba010
Kevin Matulef <matulef@gmail.com>
2019-01-15 14:22:35 -0800

    initial pass at risk adjustment calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from "@angular/core"
+import { COMMA, ENTER } from "@angular/cdk/keycodes"
+import { Component, ElementRef, ViewChild } from "@angular/core"
+import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms"
 
+import {
+  MatAutocompleteSelectedEvent,
+  MatChipInputEvent,
+  MatAutocomplete,
+  MatRadioButton,
+  MatRadioGroup,
+} from "@angular/material"
+import { Observable, Subject, of } from "rxjs"
+import { HttpClient, HttpParams } from "@angular/common/http"
+import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs/operators"
+
+class IcdCode {
+  code: string
+  description: string
+  hccs: number[]
+  is_billable: Boolean
+}
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.less"],
 })
 export class AppComponent {
-  title = "risk-adjustment-demo"
+  separatorKeysCodes: number[] = [ENTER, COMMA]
+
+  searchTerms = new Subject<string>()
+  icdCodes$: Observable<IcdCode[]>
+
+  selectedDiagnoses: string[] = []
+  filteredDiagnoses: Observable<string[]>
+
+  rafScore$: any
+
+  @ViewChild("diagnosisInput") diagnosisInput: ElementRef<HTMLInputElement>
+  @ViewChild("auto") matAutocomplete: MatAutocomplete
+
+  rafScoreForm: FormGroup
+  url = "https://7dw0imxsfi.execute-api.us-west-2.amazonaws.com/api/"
+
+  constructor(private http: HttpClient, private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.rafScoreForm = this._formBuilder.group({
+      diagnoses: [""],
+      age: ["70", Validators.required],
+      sex: [1, Validators.required],
+      model: ["cna", Validators.required],
+    })
+
+    this.icdCodes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) =>
+        this.http.get<IcdCode[]>(this.url + "icd_10_codes", {
+          params: { query: term },
+        })
+      )
+    )
+
+    this.rafScoreForm.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe(form => {
+        this.rafScore$ = this.http
+          .get(this.url + "risk_adjust", {
+            params: new HttpParams()
+              .set("diagnoses", this.selectedDiagnoses.map(d => d.split(":")).join())
+              .set("sex", form.sex)
+              .set("age", form.age)
+              .set("model", form.model),
+          })
+          .pipe(tap(x => console.log(x)))
+        //.subscribe(response => console.log(response))
+      })
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term)
+  }
+
+  add(event: MatChipInputEvent): void {
+    // Add diagnoses only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input
+      const value = event.value
+
+      // Add our diagnoses
+      if ((value || "").trim()) {
+        this.selectedDiagnoses.push(value.trim())
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = ""
+      }
+
+      this.rafScoreForm.get("diagnoses").setValue(null)
+    }
+  }
+
+  remove(diagnoses: string): void {
+    const index = this.selectedDiagnoses.indexOf(diagnoses)
+
+    if (index >= 0) {
+      this.selectedDiagnoses.splice(index, 1)
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    console.log(event)
+    this.selectedDiagnoses.push(event.option.value)
+    this.diagnosisInput.nativeElement.value = ""
+    this.rafScoreForm.get("diagnoses").setValue(null)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,35 @@ import { NgModule } from "@angular/core"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-
+import {
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatRadioModule,
+  MatInputModule,
+  MatSelectModule,
+  MatExpansionModule,
+} from "@angular/material"
+import { ReactiveFormsModule } from "@angular/forms"
+import { HttpClientModule } from "@angular/common/http"
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatRadioModule,
+    MatInputModule,
+    MatSelectModule,
+    MatExpansionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
